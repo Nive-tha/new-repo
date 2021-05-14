54ba89f7efd7f5b83853b061d289f18f6b2d70fc
malathimahendran <malamagendran@gmail.com>
2021-05-14 10:54:49 +0530

    new changes file added

---
+++ android/app/src/main/kotlin/com/example/parampara_login_reg/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.parampara_login_reg
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/HomePage.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+// import 'package:parampara_login_reg/menu.dart';
+
+class Homepage extends StatefulWidget {
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home"),
+      ),
+    );
+  }
+}
