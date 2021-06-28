cf7ffd001feeb92848492ab29de323aef0ff51f5
Nive-tha <nivem17@gmail.com>
2021-06-28 16:41:13 +0530

    update

---
+++ lib/calender/calendersecondpage.dart
@@ -0,0 +1,253 @@
+import 'package:flutter/material.dart';
+import 'package:google/LoginPage/Landingpage.dart';
+import 'package:select_form_field/select_form_field.dart';
+
+class CalendarSecondPage extends StatefulWidget {
+  @override
+  _CalendarSecondPageState createState() => _CalendarSecondPageState();
+}
+
+class _CalendarSecondPageState extends State<CalendarSecondPage> {
+  var today = DateTime.now();
+  var text;
+  final List<Map<String, dynamic>> _items = [
+    {
+      'value': 'yesterday',
+      'label': 'yesterday',
+    },
+    {
+      'value': 'today',
+      'label': 'today',
+    },
+    {
+      'value': 'tomorrow',
+      'label': 'tomorrow',
+    },
+  ];
+  @override
+  Widget build(BuildContext context) {
+    var height = MediaQuery.of(context).size.height;
+    var width = MediaQuery.of(context).size.width;
+    var bottom = kBottomNavigationBarHeight;
+    var safeArea = MediaQuery.of(context).padding.top;
+    var padding = MediaQuery.of(context).padding;
+
+    return Scaffold(
+      backgroundColor: Colors.teal,
+      body: Container(
+        height: height,
+        width: width,
+        child: Column(
+          children: [
+            Container(
+              height: ((height - bottom) * 0.15),
+              width: width,
+              color: Colors.teal,
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: ((height - bottom) * 0.15) * 0.5,
+                    width: width,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(40),
+                      ),
+                      color: Colors.white,
+                    ),
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 18.0),
+                          child: IconButton(
+                            icon: Icon(
+                              Icons.arrow_back_ios,
+                            ),
+                            iconSize: 20,
+                            color: Colors.teal,
+                            splashColor: Colors.tealAccent,
+                            onPressed: () {
+                              Navigator.pushReplacement(
+                                  context,
+                                  new MaterialPageRoute(
+                                      builder: (BuildContext context) =>
+                                          new LandingPage()));
+                            },
+                          ),
+                        ),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Text(
+                          'Calendar',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                              color: Colors.teal),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(
+                top: 20,
+                bottom: 10,
+                left: 15,
+                right: 15,
+              ),
+              height: (height - bottom) * 0.85,
+              width: width,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30),
+                  topRight: Radius.circular(30),
+                ),
+              ),
+              child: Column(
+                children: [
+                  customText(
+                    text = "Create Remainder",
+                  ),
+                  SizedBox(
+                    height: ((height - bottom) * 0.85) * 0.02,
+                  ),
+                  customText(
+                    text = "Title",
+                  ),
+                  SizedBox(
+                    height: ((height - bottom) * 0.85) * 0.02,
+                  ),
+                  Container(
+                    height: ((height - bottom) * 0.85) * 0.1,
+                    width: width - 30,
+                    child: Card(
+                      color: Colors.grey.shade100,
+                    ),
+                  ),
+                  SizedBox(
+                    height: ((height - bottom) * 0.85) * 0.02,
+                  ),
+                  Row(
+                    children: [
+                      customText(
+                        text = "Date",
+                      ),
+                      SizedBox(width: width * 0.53),
+                      customText(
+                        text = "Time",
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: ((height - bottom) * 0.85) * 0.02,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Container(
+                            height: ((height - bottom) * 0.85) * 0.07,
+                            width: width * 0.3,
+                            child: Card(
+                              color: Colors.grey.shade100,
+                              child: SelectFormField(
+                                decoration: InputDecoration(
+                                  hintText: "select",
+                                  border: InputBorder.none,
+                                  suffixIcon: Icon(Icons.arrow_drop_down),
+                                ),
+                                items: _items,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Container(
+                            height: ((height - bottom) * 0.85) * 0.07,
+                            width: width * 0.2,
+                            child: Card(
+                              color: Colors.grey.shade100,
+                            ),
+                          ),
+                          Text(
+                            ":",
+                            style: TextStyle(
+                              color: Colors.teal,
+                              fontSize: 16,
+                            ),
+                          ),
+                          Container(
+                            height: ((height - bottom) * 0.85) * 0.07,
+                            width: width * 0.2,
+                            child: Card(
+                              color: Colors.grey.shade100,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: ((height - bottom) * 0.85) * 0.02,
+                  ),
+                  customText(
+                    text = "Description",
+                  ),
+                  SizedBox(
+                    height: ((height - bottom) * 0.85) * 0.02,
+                  ),
+                  Container(
+                    height: ((height - bottom) * 0.85) * 0.25,
+                    width: width,
+                    child: Card(
+                      color: Colors.grey.shade100,
+                    ),
+                  ),
+                  SizedBox(
+                    height: ((height - bottom) * 0.85) * 0.2,
+                  ),
+                  InkWell(
+                    onTap: () {},
+                    child: Container(
+                      height: ((height - bottom) * 0.85) * 0.06,
+                      width: width / 3.5,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(
+                          Radius.elliptical(30, 30),
+                        ),
+                        color: Colors.teal,
+                      ),
+                      child: Center(
+                          child: Text(
+                        "Create",
+                        style: TextStyle(color: Colors.white),
+                      )),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget customText(text) {
+    return Text(
+      text,
+      style: TextStyle(
+        fontWeight: FontWeight.bold,
+        fontSize: 16,
+        color: Colors.teal,
+      ),
+    );
+  }
+}
---
+++ lib/events/events.dart
@@ -0,0 +1,396 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google/LoginPage/Landingpage.dart';
+import 'package:intl/intl.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Events extends StatefulWidget {
+  @override
+  _EventsState createState() => _EventsState();
+}
+
+class _EventsState extends State<Events> {
+  var postingDate = TextEditingController();
+  var postingEndDate = TextEditingController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  datePickingStart() async {
+    var today = DateTime.now();
+    print(today);
+    var date = await showDatePicker(
+      context: context,
+      initialDate: today,
+      firstDate: DateTime(DateTime.now().year - 5),
+      lastDate: DateTime(DateTime.now().year + 5),
+    );
+
+    if (date == null)
+      return;
+    else {
+      setState(() {
+        postingDate.text = DateFormat('yyyy/MM/dd').format(date);
+      });
+    }
+  }
+
+  datePickingEnd() async {
+    var today = DateTime.now();
+    print(today);
+    var date = await showDatePicker(
+      context: context,
+      initialDate: today,
+      firstDate: DateTime(DateTime.now().year - 5),
+      lastDate: DateTime(DateTime.now().year + 5),
+    );
+
+    if (date == null)
+      return;
+    else {
+      setState(() {
+        postingEndDate.text = DateFormat('yyyy/MM/dd').format(date);
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var sizeHeight = MediaQuery.of(context).size.height;
+    var sizeWidth = MediaQuery.of(context).size.width;
+    var bottom = kBottomNavigationBarHeight;
+    return Scaffold(
+        backgroundColor: Colors.teal,
+        body: Container(
+            child: Column(children: [
+          Container(
+            height: ((sizeHeight - bottom) * 0.15),
+            width: sizeWidth,
+            color: Colors.teal,
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: ((sizeHeight - bottom) * 0.15) * 0.5,
+                  width: sizeWidth,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(40),
+                      ),
+                      color: Colors.white),
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 18.0),
+                        child: IconButton(
+                          icon: Icon(
+                            Icons.arrow_back_ios,
+                          ),
+                          iconSize: 20,
+                          color: Colors.teal,
+                          splashColor: Colors.tealAccent,
+                          onPressed: () {
+                            Navigator.pushReplacement(
+                                context,
+                                new MaterialPageRoute(
+                                    builder: (BuildContext context) =>
+                                        new LandingPage()));
+                          },
+                        ),
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Text(
+                        'Events',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                            color: Colors.teal),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Expanded(
+              child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    image: DecorationImage(
+                      image: AssetImage("assets/matrimony.jpeg"),
+                      fit: BoxFit.cover,
+                    ),
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(30),
+                        topRight: Radius.circular(30)),
+                  ),
+                  height: sizeHeight * 0.74,
+                  width: sizeWidth,
+                  child: SingleChildScrollView(
+                      child: Form(
+                          child: Column(children: [
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      width: 280,
+                      height: 50,
+                      child: TextFormField(
+                          // controller: username,
+                          decoration: InputDecoration(
+                              prefixIcon: Icon(Icons.person),
+                              filled: true,
+                              fillColor: Colors.white,
+                              // contentPadding: const EdgeInsets.symmetric(horizontal: 4.0),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(30.0)),
+                                borderSide: BorderSide(
+                                    color: Colors.teal.shade50, width: 1),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(40.0)),
+                                borderSide: BorderSide(color: Colors.teal),
+                              ),
+                              hintText: "   Event name",
+                              hintStyle: TextStyle(
+                                color: Colors.teal,
+                                fontSize: 15,
+                                // fontWeight: FontWeight.bold,
+                              ))),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      width: 280,
+                      height: 50,
+                      child: TextFormField(
+                          controller: postingDate,
+                          decoration: InputDecoration(
+                              prefixIcon: InkWell(
+                                  onTap: datePickingStart,
+                                  child: Icon(Icons.date_range)),
+                              filled: true,
+                              fillColor: Colors.white,
+                              // contentPadding: const EdgeInsets.symmetric(horizontal: 4.0),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(30.0)),
+                                borderSide: BorderSide(
+                                    color: Colors.teal.shade50, width: 1),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(40.0)),
+                                borderSide: BorderSide(color: Colors.teal),
+                              ),
+                              hintText: "   Date",
+                              hintStyle: TextStyle(
+                                color: Colors.teal,
+                                fontSize: 15,
+                                // fontWeight: FontWeight.bold,
+                              ))),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      width: 280,
+                      height: 50,
+                      child: TextFormField(
+                          // controller: username,
+                          decoration: InputDecoration(
+                              prefixIcon: Icon(Icons.supervised_user_circle),
+                              filled: true,
+                              fillColor: Colors.white,
+                              // contentPadding: const EdgeInsets.symmetric(horizontal: 4.0),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(30.0)),
+                                borderSide: BorderSide(
+                                    color: Colors.teal.shade50, width: 1),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(40.0)),
+                                borderSide: BorderSide(color: Colors.teal),
+                              ),
+                              hintText: "   Time",
+                              hintStyle: TextStyle(
+                                color: Colors.teal,
+                                fontSize: 15,
+                                // fontWeight: FontWeight.bold,
+                              ))),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      width: 280,
+                      height: 50,
+                      child: TextFormField(
+                          // controller: username,
+                          decoration: InputDecoration(
+                              prefixIcon: Icon(Icons.title),
+                              filled: true,
+                              fillColor: Colors.white,
+                              // contentPadding: const EdgeInsets.symmetric(horizontal: 4.0),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(30.0)),
+                                borderSide: BorderSide(
+                                    color: Colors.teal.shade50, width: 1),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(40.0)),
+                                borderSide: BorderSide(color: Colors.teal),
+                              ),
+                              hintText: "   Place",
+                              hintStyle: TextStyle(
+                                color: Colors.teal,
+                                fontSize: 15,
+                                // fontWeight: FontWeight.bold,
+                              ))),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      width: 280,
+                      height: 50,
+                      child: TextFormField(
+                          // controller: username,
+                          decoration: InputDecoration(
+                              prefixIcon: Icon(Icons.format_list_numbered),
+                              filled: true,
+                              fillColor: Colors.white,
+                              // contentPadding: const EdgeInsets.symmetric(horizontal: 4.0),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(30.0)),
+                                borderSide: BorderSide(
+                                    color: Colors.teal.shade50, width: 1),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(40.0)),
+                                borderSide: BorderSide(color: Colors.teal),
+                              ),
+                              hintText: "   Contact Number",
+                              hintStyle: TextStyle(
+                                color: Colors.teal,
+                                fontSize: 15,
+                                // fontWeight: FontWeight.bold,
+                              ))),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      width: 280,
+                      height: 50,
+                      child: TextFormField(
+                          // controller: username,
+                          decoration: InputDecoration(
+                              prefixIcon: InkWell(
+                                  onTap: datePickingEnd,
+                                  child: Icon(Icons.date_range)),
+                              filled: true,
+                              fillColor: Colors.white,
+                              // contentPadding: const EdgeInsets.symmetric(horizontal: 4.0),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(30.0)),
+                                borderSide: BorderSide(
+                                    color: Colors.teal.shade50, width: 1),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(40.0)),
+                                borderSide: BorderSide(color: Colors.teal),
+                              ),
+                              hintText: "   Upload Picture",
+                              hintStyle: TextStyle(
+                                color: Colors.teal,
+                                fontSize: 15,
+                                // fontWeight: FontWeight.bold,
+                              ))),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      width: sizeWidth,
+                      height: 100,
+                      padding: EdgeInsets.only(left: 28, right: 28),
+                      child: Card(
+                        color: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.elliptical(15, 15),
+                          ),
+                        ),
+                        child: TextFormField(
+                            // maxLines: 3,
+                            // maxLength: 10,
+
+                            // controller: username,
+                            decoration: InputDecoration(
+                                // prefixIcon: Icon(Icons.person),
+                                // filled: true,
+                                // fillColor: Colors.white,
+                                border: InputBorder.none,
+                                // contentPadding: const EdgeInsets.symmetric(horizontal: 4.0),
+                                // enabledBorder: OutlineInputBorder(
+                                //   borderRadius:
+                                //       BorderRadius.all(Radius.circular(30.0)),
+                                //   borderSide: BorderSide(
+                                //       color: Colors.teal.shade50, width: 1),
+                                // ),
+                                // focusedBorder: OutlineInputBorder(
+                                //   borderRadius:
+                                //       BorderRadius.all(Radius.circular(40.0)),
+                                //   borderSide: BorderSide(color: Colors.teal),
+                                // ),
+                                hintText: "   Description",
+                                hintStyle: TextStyle(
+                                  color: Colors.teal,
+                                  fontSize: 15,
+                                  // fontWeight: FontWeight.bold,
+                                ))),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      height: 45,
+                      width: 100,
+                      decoration: BoxDecoration(
+                        color: Colors.teal,
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(40),
+                        ),
+                      ),
+                      child: Center(
+                        child: Text(
+                          'Post',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ])))))
+        ])));
+  }
+}
