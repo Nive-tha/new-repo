0cb0605947eb8ecf66bc6833207ea299ad395825
malathimahendran <malamagendran@gmail.com>
2021-07-14 15:52:13 +0530

    tree page added

--- lib/treepage/tree1.dart
+++ lib/treepage/tree1.dart
@@ -11,21 +11,50 @@ class _Tree1State extends State<Tree1> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.teal[800],
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Icon(
-              Icons.account_circle,
-              size: 50.0,
-              color: Colors.white,
-            ),
-            Text("FamilyTree",style:TextStyle(color:Colors.white,fontWeight: FontWeight.bold,fontSize:30)),
-            Text("Back",style:TextStyle(color:Colors.white,fontSize: 10)),
-          ],
-        ),
+      body:  Container(
+        child: Column(
+     
+            children: [
+              Expanded(
+                              child: Container(
+                  color: Colors.teal[700],
+                  child: Padding(
+                    padding: const EdgeInsets.only(top:20,bottom:20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Icon(
+                          Icons.account_circle,
+                          size: 50.0,
+                          color: Colors.white,
+                        ),
+                        Text("FamilyTree",style:TextStyle(color:Colors.white,fontWeight: FontWeight.bold,fontSize:25)),
+                        Text("Back",style:TextStyle(color:Colors.white,fontSize: 10)),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              
+              Expanded(
+                flex: 8,
+                              child: Container(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+            
+                      children: [
+                        Icon(Icons.person_add),
+                        Text("Add You in Tree")
+                      ],
+                    ),
+
+                  ),
+              )
+            ],
+          ),
       ),
+    
+      
     );
   }
 }
